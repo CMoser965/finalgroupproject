f59d72f61751605e83b31a1f4abde17219899cec
Christian Moser <48960224+CMoser965@users.noreply.github.com>
2021-10-28 22:20:35 -0500

    Add functionality for initializing a server

--- src/data.c
+++ src/data.c
@@ -24,3 +24,37 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#include "include/data.h"
+
+void error(char *error) {
+    printf("Error: %s", error);
+    exit(1);
+}
+
+
+int init_server() {
+    // initialize vars
+    int connection fd;
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t len = sizeof(cli_addr);
+
+    // setup server address
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    // setup client connectivity
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_socket < 0) error("socket cannot be initialized.\n");
+
+    // bind sock to serv addr
+    int sock_bind_status = bind(server_socket, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if(sock_bind_status < 0) error("socket cannot be binded\n");
+
+    // listen ... up to 100 servers
+    int listen_status = listen(server_socket, 100);
+    if(listen_status) error("server could not listen for client\n");
+
+    return accept(server_socket, (struct sockaddr*)&cli_addr, &len);
+
+}
---
+++ src/include/data.h
@@ -0,0 +1,6 @@
+#define PORT 4000
+#define BUFFER_SIZE 2048
+
+void error(char *error);
+
+void init_server();
