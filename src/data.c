48a828d054c22a45a5632b93c1223144f511e230
Christian Moser <48960224+CMoser965@users.noreply.github.com>
2021-10-28 21:56:20 -0500

    Upload data server files

---
+++ src/data.c
@@ -0,0 +1,26 @@
+/**
+ * Author:  Christian Moser
+ * Date:    10-28-21
+ * Description: Driver file for data server.
+ * **/
+
+// standard libs
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+// threading libs
+#include <pthread.h>
+#include <signal.h>
+
+// sys call libs
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+
+// net standard libs
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
